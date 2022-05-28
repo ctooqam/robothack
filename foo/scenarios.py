293f98ac2d4cd1f7fa6f97a1a5f438536afb8e05
Linus <linus@eqe.se>
2022-05-28 17:08:57 +0200

    .

--- foo/scenarios.py
+++ foo/scenarios.py
@@ -25,9 +25,10 @@ def scenarioA2(ev3, left_motor, right_motor, front_motor, line_sensor, other_sen
   robot.turn(90)
   robot.straight(80)
   move_straight_until(other_sensor, robot, sharp_white_black_edge)
-  rotate_ccw_until(other_sensor, robot, lambda measurements: sharp_white_black_edge(measurements, reversed=True), speed=20)
+
+  rotate_ccw_until(other_sensor, robot, lambda measurements: sharp_white_black_edge(measurements, reversed=True), speed=25)
 
 
 def scenarioA3(ev3, left_motor, right_motor, front_motor, line_sensor, other_sensor, robot, obstacle_sensor):
-    follow_line_until(other_sensor, line_sensor, robot, lambda x: stop_at_obstacle(x, obstacle_sensor, 200), side_factor=-1)   
+    follow_line_until(other_sensor, line_sensor, robot, lambda x: stop_at_obstacle(x, obstacle_sensor, 200), side_factor=-1, PROPORTIONAL_GAIN=1.1)   
 
