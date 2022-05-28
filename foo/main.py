48c2ba13a6a3306a56bb6a511e8e554d31ab7619
Linus <linus@eqe.se>
2022-05-28 14:11:23 +0200

    Added a callback for following line until..

--- foo/main.py
+++ foo/main.py
@@ -15,7 +15,7 @@ from pybricks.parameters import Port
 from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 
-from utils import passed_white_black_white
+from utils import detect_black_line_callback, follow_line_until
 
 ev3 = EV3Brick()
 
@@ -30,11 +30,6 @@ other_sensor = ColorSensor(Port.S1)
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-# Calculate the light threshold. Choose values based on your measurements.
-BLACK = 9
-WHITE = 85
-threshold = (BLACK + WHITE) / 2
-
 # Set the drive speed at 100 millimeters per second.
 DRIVE_SPEED = 100
 
@@ -42,45 +37,5 @@ DRIVE_SPEED = 100
 # percentage point of light deviating from the threshold, we set the turn
 # rate of the drivebase to 1.2 degrees per second.
 
-# For example, if the light value deviates from the threshold by 10, the robot
-# steers at 10*1.2 = 12 degrees per second.
-PROPORTIONAL_GAIN = 1.2
-measurements = []
-search = 'white'
-# Start following the line endlessly.
-
-# Vi sparar de K senaste mätvärdena för att detektera när vi passerat ett vitt-svart-vitt område.
-# Beroede på robotens hastighet så kommer vi ha X mätvärden som tas över det mörka området. 
-# x ska motsvara ca 30-40% av K för att passed_white_black_white ska bli nöjd.
-dark_line_width = 25  # mm
-nbr_dark_samples = 100 * dark_line_width / DRIVE_SPEED
-K = int(nbr_dark_samples / 0.35)
-
-measurements = []
-
-while True:
-
-    measurements.append(other_sensor.reflection())
-
-    if len(measurements) > K and passed_white_black_white(measurements[len(measurements)-K:]):
-        ev3.speaker.beep()
-        print("Detected white-black-white")
-
-    # Calculate the deviation from the threshold.
-    deviation = line_sensor.reflection() - threshold
-    measurements.append(other_sensor.reflection())
-    # ev3.screen.print(other_sensor.reflection())
-    #deviation = line_sensor.reflection() - threshold
-    # print(line_sensor.reflection())
-    # ev3.screen.print(line_sensor.reflection())
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
-
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
-
-    if len(measurements) > 10000:
-        measurements = measurements[5000:]
+follow_line_until(detect_black_line_callback)
+print("Done!")
--- foo/utils.py
+++ foo/utils.py
@@ -14,3 +14,48 @@ def passed_white_black_white(measurements):
     dark = mean(measurements[start_dark:end_dark])
     return bright_1 > 2 * dark and bright_2 > 2 * dark
 
+
+def detect_black_line_callback(measurements)
+    # Vi sparar de K senaste mätvärdena för att detektera när vi passerat ett vitt-svart-vitt område.
+    # Beroede på robotens hastighet så kommer vi ha X mätvärden som tas över det mörka området. 
+    # x ska motsvara ca 30-40% av K för att passed_white_black_white ska bli nöjd.
+    dark_line_width = 25  # mm
+    nbr_dark_samples = 100 * dark_line_width / DRIVE_SPEED
+    K = int(nbr_dark_samples / 0.35)
+
+    return len(measurements) > K and passed_white_black_white(measurements[len(measurements)-K:]):
+
+
+def follow_line_until(line_sensor, other_sensor, callback, *, DRIVE_SPEED = 100, PROPORTIONAL_GAIN = 1.2):
+    """
+    Callback takes measurements from the second sensor. Returns True when we are finnished.
+    
+      DRIVE_SPEED       - Set the drive speed at 100 millimeters per second.
+      PROPORTIONAL_GAIN - For example, if the light value deviates from the threshold by 10, the robot
+                          steers at 10*1.2 = 12 degrees per second.
+    """
+    
+    # Calculate the light threshold. Choose values based on your measurements.
+    BLACK = 9
+    WHITE = 85
+    threshold = (BLACK + WHITE) / 2
+
+    measurements = []
+
+    while True:
+        measurements.append(other_sensor.reflection())
+        if len(measurements) > 10000:
+            measurements = measurements[5000:]
+
+        if callback(measurements):
+            break
+
+        deviation = line_sensor.reflection() - threshold
+        turn_rate = PROPORTIONAL_GAIN * deviation
+
+        # Set the drive base speed and turn rate.
+        robot.drive(DRIVE_SPEED, turn_rate)
+
+        # You can wait for a short time or do other things in this loop.
+        wait(10)
+
